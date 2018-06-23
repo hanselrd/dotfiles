8cdf076022e1a78aed05a2c511c3fd66c6b3a316
Hansel De La Cruz <18725263+hanselrd@users.noreply.github.com>
2018-06-22 23:35:33 -0400

    added tmux configuration and install script

---
+++ install.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+sudo dnf install i3 i3lock feh network-manager-applet xbacklight -y
+
+cp -R wallpaper.jpg ~/Pictures/wallpaper.jpg
+
+cp -R .config/i3/config ~/.config/i3/config
+cp -R .config/i3status/config ~/.config/i3status/config
+
+cp -R .fonts ~/.fonts
+
+cp -R .vimrc ~/.vimrc
+vim +PluginInstall +qall
+
+cp -R .tmux.conf ~/.tmux.conf
+
+cp -R .gitconfig ~/.gitconfig
+echo "Make sure to add your email/name to ~/.gitconfig"
+
+sudo cp -R etc/X11/xorg.conf /etc/X11/xorg.conf
