a0c95fa7001a95501c171208a263512d261b027d
Hansel De La Cruz <18725263+hanselrd@users.noreply.github.com>
2018-08-31 01:06:35 -0400

    fix union bug and line endings

--- .gdb/offsetsof.py
+++ .gdb/offsetsof.py
@@ -30,8 +30,9 @@ class Offsets(gdb.Command):
             if last_field != None and last_field.bitsize == 0 and ((last_field.bitpos / 8) + last_field.type.sizeof) != (field.bitpos / 8):
                 tmp1 = int(last_field.bitpos / 8) + last_field.type.sizeof
                 tmp2 = int(field.bitpos / 8) - tmp1
+                code = stype.strip_typedefs().code
                 # unions cause a padding false-positive
-                if tmp2 > 0:
+                if code is not gdb.TYPE_CODE_UNION:
                     print(textwrap.indent('__pad__; => {} [{} padding]'.format(tmp1, tmp2), ' ' * 4 * (indent + 1)))
                     padding += tmp2
             code = field.type.strip_typedefs().code
@@ -45,8 +46,10 @@ class Offsets(gdb.Command):
         if last_field != None and last_field.bitsize == 0 and ((last_field.bitpos / 8) + last_field.type.sizeof) != stype.sizeof:
             tmp4 = int(last_field.bitpos / 8) + last_field.type.sizeof
             tmp5 = stype.sizeof - tmp4
-            print(textwrap.indent('__pad__; => {} [{} padding]'.format(tmp4, tmp5), ' ' * 4 * (indent + 1)))
-            padding += tmp5
+            code = stype.strip_typedefs().code
+            if code is not gdb.TYPE_CODE_UNION:
+                print(textwrap.indent('__pad__; => {} [{} padding]'.format(tmp4, tmp5), ' ' * 4 * (indent + 1)))
+                padding += tmp5
 
         if sfield is None:
             print(textwrap.indent('}}; [{}] ({} padding)'.format(stype.sizeof, padding), ' ' * 4 * indent))
