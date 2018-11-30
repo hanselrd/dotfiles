95119b1717ccf0b7339e3e8673aadc41f92145b6
Hansel De La Cruz <18725263+hanselrd@users.noreply.github.com>
2018-11-30 18:39:21 -0500

    add missing extensions

--- vscode_extensions.sh
+++ vscode_extensions.sh
@@ -7,6 +7,7 @@ code --install-extension esbenp.prettier-vscode
 code --install-extension josef.rouge-theme
 code --install-extension jpoissonnier.vscode-styled-components
 code --install-extension kumar-harsh.graphql-for-vscode
+code --install-extension liximomo.remotefs
 code --install-extension mikestead.dotenv
 code --install-extension ms-python.python
 code --install-extension ms-vscode.cpptools
@@ -14,12 +15,16 @@ code --install-extension nepaul.editorconfiggenerator
 code --install-extension PKief.material-icon-theme
 code --install-extension primalmotion.primal
 code --install-extension rust-lang.rust
+code --install-extension shd101wyy.markdown-preview-enhanced
 code --install-extension sidneys1.gitconfig
 code --install-extension slevesque.shader
 code --install-extension slevesque.vscode-hexdump
 code --install-extension smkamranqadri.vscode-bolt-language
 code --install-extension toba.vsfire
 code --install-extension twxs.cmake
+code --install-extension VisualStudioExptTeam.vscodeintellicode
+code --install-extension vscode-ext.sync-rsync
 code --install-extension vscodevim.vim
+code --install-extension waderyan.gitblame
 code --install-extension xaver.clang-format
 code --install-extension ymotongpoo.licenser
