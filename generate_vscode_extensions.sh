08410a01aa7753cbb92a72d76eaaabf499d015f0
Hansel De La Cruz <hansel809@hotmail.com>
2019-05-14 17:32:15 -0400

    add new vscode and extensions

--- generate_vscode_extensions.sh
+++ generate_vscode_extensions.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-printf "#!/usr/bin/env bash\n\n" > vscode_extensions.sh && code --list-extensions | xargs -L 1 echo "code --install-extension" >> vscode_extensions.sh
+printf "#!/usr/bin/env bash\n\n" > vscode_extensions.sh && code-insiders --list-extensions | xargs -L 1 echo "code-insiders --install-extension" >> vscode_extensions.sh
 chmod +x vscode_extensions.sh
