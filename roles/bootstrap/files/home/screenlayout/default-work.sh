1187546c467b685726137d7c2c373e1007404471
Hansel De La Cruz <hansel809@hotmail.com>
2020-01-31 13:10:03 -0500

    fix xrandr script

--- roles/bootstrap/files/home/screenlayout/default-work.sh
+++ roles/bootstrap/files/home/screenlayout/default-work.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output HDMI2 --mode 1920x1080 --pos 1920x0 --rotate left --output HDMI1 --mode 1920x1080 --pos 0x400 --rotate normal --output VIRTUAL1 --off --output DP1 --off
+xrandr --output HDMI-2 --mode 1920x1080 --pos 1920x0 --rotate left --output HDMI-1 --mode 1920x1080 --pos 0x400 --rotate normal
