fc3ee38fa96a3d478b3bf01d6e2208e8afd34ec7
Hansel De La Cruz <hansel809@hotmail.com>
2020-05-15 12:08:28 -0400

    add arch bootstrap script

--- scripts/arch_bootstrap.sh
+++ scripts/arch_bootstrap.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
 # Assumes the following setup
 
@@ -12,116 +12,149 @@
 #     (/.swap)      @swap
 #     (/.keys)      @keys
 
-# Open LUKS
+echo "Open LUKS container on /dev/sda2"
 cryptsetup luksOpen /dev/sda2 cryptbtrfs
 
-# Mount partitions
+echo "Mount partitions"
 mount -o compress=lzo,subvol=@ /dev/mapper/cryptbtrfs /mnt
 mkdir -p /mnt/{efi,home,.shared,.swap,.keys}
-chmod 777 /mnt/.shared
 mount /dev/sda1 /mnt/efi
 mount -o compress=lzo,subvol=@home /dev/mapper/cryptbtrfs /mnt/home
 mount -o compress=lzo,subvol=@shared /dev/mapper/cryptbtrfs /mnt/.shared
 mount -o compress=lzo,subvol=@swap /dev/mapper/cryptbtrfs /mnt/.swap
 mount -o compress=lzo,subvol=@keys /dev/mapper/cryptbtrfs /mnt/.keys
+chmod 777 /mnt/.shared
 
-# Verify the boot mode
+echo "Verify the boot mode"
 ls /sys/firmware/efi/efivars
 
-# Connect to the internet
+echo "Connect to the internet"
 wifi-menu
 
-# Update the system clock
+echo "Update the system clock"
 timedatectl set-ntp true
 
-# Create swapfile
+echo "Create swapfile"
 truncate -s 0 /mnt/.swap/swapfile
 chattr +C /mnt/.swap/swapfile
 if [ "$(findmnt -no FSTYPE -T /mnt/.swap)" == "btrfs" ]; then
     btrfs property set /mnt/.swap/swapfile compression none
 fi
-fallocate -l $(free -g | head -n 2 | tail -n 1 | awk '{printf("%d\n", $2+1)}')G /mnt/.swap/swapfile
+fallocate -l $(free -g | grep "Mem" | awk '{printf("%d\n", $2+1)}')G /mnt/.swap/swapfile
 chmod 600 /mnt/.swap/swapfile
 mkswap /mnt/.swap/swapfile
 swapon /mnt/.swap/swapfile
 
-# Select the mirrors
+echo "Select the mirrors"
 pacman -S reflector --noconfirm
 reflector --country "United States" --age 12 --sort rate --save /etc/pacman.d/mirrorlist
 pacman -Syyy --noconfirm
 
-# Install essential packages
-pacstrap /mnt base base-devel linux linux-headers linux-firmware vim
+echo "Install essential packages"
+pacstrap /mnt \
+    ansible \
+    base \
+    base-devel \
+    btrfs-progs \
+    efibootmgr \
+    git \
+    grub \
+    grub-btrfs \
+    intel-ucode \
+    linux \
+    linux-firmware \
+    linux-headers \
+    network-manager-applet \
+    networkmanager \
+    os-prober \
+    snap-pac \
+    snapper \
+    vim
 
-# Fstab
+echo "Fstab"
 genfstab -U /mnt >> /mnt/etc/fstab
 
-# Chroot
-arch-chroot /mnt
+cat <<EOF >> /mnt/bootstrap.sh
+echo "Time zone"
+ln -sf /usr/share/zoneinfo/America/New_York /etc/localtime
+hwclock --systohc
 
-##############################################################################
-##### from inside /mnt (arch-chroot)
-###############################################################################
+echo "Localization"
+sed -i 's/#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/g' /etc/locale.gen
+sed -i 's/#en_US ISO-8859-1/en_US ISO-8859-1/g' /etc/locale.gen
+locale-gen
+echo "LANG=en_US.UTF-8" >> /etc/locale.conf
+
+echo "Network configuration"
+echo "arch" >> /etc/hostname
+printf "127.0.0.1\tlocalhost\n" >> /etc/hosts
+printf "::1\t\tlocalhost\n" >> /etc/hosts
+printf "127.0.1.1\tarch.localdomain\tarch\n" >> /etc/hosts
+systemctl enable NetworkManager
+
+echo "Create LUKS key"
+dd if=/dev/random of=/.keys/cryptbtrfs.keyfile iflag=fullblock bs=512 count=8
+chmod 000 /.keys/cryptbtrfs.keyfile
+cryptsetup luksAddKey /dev/sda2 /.keys/cryptbtrfs.keyfile
+
+echo "Initramfs"
+sed -i 's/^BINARIES=(.*)/BINARIES=(\/usr\/bin\/btrfs)/g' /etc/mkinitcpio.conf
+sed -i 's/^FILES=(.*)/FILES=(\/.keys\/cryptbtrfs.keyfile)/g' /etc/mkinitcpio.conf
+sed -i 's/^HOOKS=(.*)/HOOKS=(base systemd autodetect keyboard modconf block sd-encrypt filesystems fsck)/g' /etc/mkinitcpio.conf
+mkinitcpio -P
+
+echo "Root password"
+passwd
+
+echo "User password"
+useradd -mG wheel delacruz
+passwd delacruz
+
+echo "Update sudoers for wheel group"
+sed -i 's/# %wheel ALL=(ALL) ALL/%wheel ALL=(ALL) ALL/g' /etc/sudoers
+
+echo "Snapper"
+snapper --no-dbus --config=root create-config /
+snapper --no-dbus --config=root create --description="Initial"
+snapper --no-dbus --config=home create-config /home
+snapper --no-dbus --config=home create --description="Initial"
+
+echo "Boot loader"
+uuid=$(lsblk -no UUID /dev/sda2 | head -n 1)
+sed -i 's/loglevel=3 quiet/loglevel=3/g' /etc/default/grub
+sed -i 's/GRUB_CMDLINE_LINUX=""/GRUB_CMDLINE_LINUX="rd.luks.name=$uuid=cryptbtrfs rd.luks.key=$uuid=/.keys/cryptbtrfs.keyfile"/g' /etc/default/grub
+sed -i 's/#GRUB_ENABLE_CRYPTODISK=y/GRUB_ENABLE_CRYPTODISK=y/g' /etc/default/grub
+grub-install --target=x86_64-efi --efi-directory=/efi --bootloader-id=GRUB --recheck
+grub-mkconfig -o /boot/grub/grub.cfg
+
+echo "Set Pacman Options"
+sed -i 's/#Color/Color/g' /etc/pacman.conf
+sed -i 's/#CheckSpace/CheckSpace/g' /etc/pacman.conf
+sed -i 's/#VerbosePkgLists/VerbosePkgLists/g' /etc/pacman.conf
+
+echo "Exit chroot"
+exit
+EOF
+
+echo "Chroot"
+arch-chroot /mnt /bootstrap.sh
+
+echo "Cleanup chroot bootstrap"
+rm -rf /mnt/bootstrap.sh
 
 # Install yay
-cd /.shared
-git clone https://aur.archlinux.org/yay.git
-cd yay
-makepkg -si --noconfirm
+# cd /.shared
+# git clone https://aur.archlinux.org/yay.git
+# cd yay
+# makepkg -si --noconfirm
 
-# # Change to filesystem mounted under /mnt
-# chroot /mnt
+# Update system
+# sudo pacman -Syu
+# sudo pacman -Fy
 
-# # Create swapfile
-# truncate -s 0 /.swap/swapfile
-# chattr +C /.swap/swapfile
-# btrfs property set /.swap/swapfile compression none
-# fallocate -l 4G /.swap/swapfile
-# chmod 600 /.swap/swapfile
-# mkswap /.swap/swapfile
-# swapon /.swap/swapfile
-# echo "/.swap/swapfile                           none                    swap    defaults 0 0" >> /etc/fstab
-# systemctl daemon-reload
-
-# # Create keys
-# dd if=/dev/random of=/.keys/cryptbtrfs.keyfile iflag=fullblock bs=512 count=4
-# chmod 000 /.keys/cryptbtrfs.keyfile
-# cryptsetup luksDump /dev/sda2
-# cryptsetup luksAddKey /dev/sda2 /.keys/cryptbtrfs.keyfile
-# cryptsetup luksDump /dev/sda2
-# echo 'install_items+="/.keys/cryptbtrfs.keyfile"' >> /etc/dracut.conf
-# sed -i 's/none/\/.keys\/cryptbtrfs.keyfile/g' /etc/crypttab
-# dracut --force
-
-# # Check if keys were added to initramfs
-# # mkdir -p /tmp/i
-# # cd /tmp/i
-# # file /boot/initramfs-$(uname -r).img
-# # cpio -ivdF /boot/initramfs-$(uname -r).img
-# # dd if=/boot/initramfs-$(uname -r).img of=initramfs-$(uname -r).img bs=512 skip=N
-# # zcat initramfs-$(uname -r).img | cpio -ivd
-# # ll .keys
-# # cd ..
-
-# # Install snapper
-# dnf install snapper python3-dnf-plugin-snapper
-# snapper --config=root create-config /
-# snapper --config=root create --description="Initial"
-# snapper --config=home create-config /home
-# snapper --config=home create --description="Initial"
-
-# # Reinstall grub
-# dnf reinstall grub2-efi shim grub2-tools
-# echo "GRUB_ENABLE_CRYPTODISK=y" >> /etc/default/grub
-# grub2-mkconfig -o /boot/efi/EFI/fedora/grub.cfg
-
-# # Install git and ansible
-# dnf install git ansible -y
-
-# # Set up mounts
-# # Set up dnf fastestmirror and deltarpm
-# # Set up grub
-# # Install git and ansible
-# # Reboot
-# # Bootstrap
-# # Change /.swap/swapfile size
+# Bootstrap dotfiles
+# cd /.shared
+# git clone https://hanselrd@github.com/hanselrd/dotfiles.git
+# cd dotfiles
+# Add roles/bootstrap/vars/main.yml variables
+# sudo ansible-playbook localhost.yml
