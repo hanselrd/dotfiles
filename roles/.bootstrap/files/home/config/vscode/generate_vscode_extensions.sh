8425a14bf2a3b470880702e90c2a3056fdba3be3
Hansel De La Cruz <hansel809@hotmail.com>
2021-01-04 12:16:24 -0500

    remove old files

--- roles/.bootstrap/files/home/config/vscode/generate_vscode_extensions.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-printf "#!/usr/bin/env bash\n\n" > vscode_extensions.sh && code-insiders --list-extensions | xargs -L 1 echo "code-insiders --install-extension" >> vscode_extensions.sh
-chmod +x vscode_extensions.sh
--- roles/.bootstrap/files/home/screenlayout/default-work.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-xrandr --output HDMI-2 --mode 1920x1080 --pos 1920x0 --rotate left --output HDMI-1 --mode 1920x1080 --pos 0x400 --rotate normal
