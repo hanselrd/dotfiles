db5b2d8eabf6b7b0430d1c466baf16b1f8a160a6
Hansel De La Cruz <18725263+hanselrd@users.noreply.github.com>
2022-08-23 21:34:13 -0400

    add initial Role struct

--- vnext/dotfiles-ansible/src/role.rs
+++ vnext/dotfiles-ansible/src/role.rs
@@ -1,4 +1,20 @@
+use crate::*;
 use serde::Serialize;
 
-#[derive(Debug, Default, Clone, PartialEq, Eq, Serialize)]
-pub struct Role {}
+#[derive(Debug, Default, Clone, PartialEq, Serialize)]
+pub struct Role {
+    // main.yml
+    pub tasks: Vec<Task>,
+    // main.yml
+    pub handlers: Vec<String>,
+    // template.txt.j2
+    pub templates: Vec<String>,
+    // file.txt
+    pub files: Vec<String>,
+    // main.yml
+    pub vars: Vec<String>,
+    // main.yml
+    pub defaults: Vec<String>,
+    // main.yml
+    pub meta: Vec<String>,
+}
