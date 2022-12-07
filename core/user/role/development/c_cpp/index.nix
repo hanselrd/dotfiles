48676740f75f6617673466c7dd974d2e710a0f9b
Hansel De La Cruz <18725263+hanselrd@users.noreply.github.com>
2022-12-07 18:55:56 -0500

    clean up ccache config

--- core/user/role/development/c_cpp/index.nix
+++ core/user/role/development/c_cpp/index.nix
@@ -22,6 +22,8 @@
     CMAKE_BUILD_TYPE = "Release";
     CPM_SOURCE_CACHE = "~/.cache/CPM";
     CCACHE_COMPRESS = "true";
+    CCACHE_COMPRESSLEVEL = "6";
+    CCACHE_MAXSIZE = "30G";
     CCACHE_SLOPPINESS = "pch_defines,time_macros";
   };
 }
