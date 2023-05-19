7d21596c5e860fb65ad23dd80838443cc7c73a66
Hansel De La Cruz <18725263+hanselrd@users.noreply.github.com>
2023-05-19 13:52:00 -0400

    fix home-manager-eject.sh

--- scripts/home-manager-eject.sh
+++ scripts/home-manager-eject.sh
@@ -23,12 +23,13 @@ nix-store -qR "$HME_PACKAGE_2" | xargs -L1 basename > "$HME_DEP_FILE.2"
 cat $HME_DEP_FILE.* | sort | uniq > "$HME_DEP_FILE"
 find /nix/store -depth -print | grep -Ff "$HME_DEP_FILE" | cpio -ov > "$HME_CPIO_FILE"
 sed -i "s@/nix/store@$HME_STORE_DIR@g" "$HME_CPIO_FILE"
-cpio -idmvD "$HME_STORE_DIR" < "$HME_CPIO_FILE"
+mkdir -p "$HME_STORE_DIR"
+cpio -idmv < "$HME_CPIO_FILE"
 HME_PACKAGE_NEW=$(echo "$HME_PACKAGE" | sed "s@/nix/store@$HME_STORE_DIR@g")
 find "$HME_PACKAGE_NEW/home-files/" -type d -exec chmod u+w {} \;
 cp -av "$HME_PACKAGE_NEW/home-files/." "$HME_HOME_DIR/"
 HME_PACKAGE_2_NEW=$(echo "$HME_PACKAGE_2" | sed "s@/nix/store@$HME_STORE_DIR@g")
-ln -sfF "$HME_PACKAGE_2_NEW" "$HME_HOME_DIR/.nix-profile"
+ln -snfF "$HME_PACKAGE_2_NEW" "$HME_HOME_DIR/.nix-profile"
 
 echo "$HME_TEMP_DIR"
 
