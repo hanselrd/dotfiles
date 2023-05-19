21671524f90ecd84527ca5a43a4ea74786f1e4be
Hansel De La Cruz <18725263+hanselrd@users.noreply.github.com>
2023-05-19 09:21:37 -0400

    fix home-manager-eject.sh

--- scripts/home-manager-eject.sh
+++ scripts/home-manager-eject.sh
@@ -17,13 +17,18 @@ HME_CPIO_FILE="$HME_TEMP_DIR/hme.cpio"
 
 nix build --no-link ".#homeConfigurations.$HME_HOME_CONFIGURATION.activationPackage" --impure --extra-experimental-features "nix-command flakes"
 HME_PACKAGE=$(nix path-info ".#homeConfigurations.$HME_HOME_CONFIGURATION.activationPackage" --impure --extra-experimental-features "nix-command flakes")
-nix-store -qR "$HME_PACKAGE" | xargs -L1 basename > "$HME_DEP_FILE"
+nix-store -qR "$HME_PACKAGE" | xargs -L1 basename > "$HME_DEP_FILE.1"
+HME_PACKAGE_2=$(readlink -f "$HOME/.nix-profile")
+nix-store -qR "$HME_PACKAGE_2" | xargs -L1 basename > "$HME_DEP_FILE.2"
+cat $HME_DEP_FILE.* | sort | uniq > "$HME_DEP_FILE"
 find /nix/store -depth -print | grep -Ff "$HME_DEP_FILE" | cpio -ov > "$HME_CPIO_FILE"
 sed -i "s@/nix/store@$HME_STORE_DIR@g" "$HME_CPIO_FILE"
 cpio -idmvD "$HME_STORE_DIR" < "$HME_CPIO_FILE"
 HME_PACKAGE_NEW=$(echo "$HME_PACKAGE" | sed "s@/nix/store@$HME_STORE_DIR@g")
 find "$HME_PACKAGE_NEW/home-files/" -type d -exec chmod u+w {} \;
 cp -av "$HME_PACKAGE_NEW/home-files/." "$HME_HOME_DIR/"
+HME_PACKAGE_2_NEW=$(echo "$HME_PACKAGE_2" | sed "s@/nix/store@$HME_STORE_DIR@g")
+ln -sfF "$HME_PACKAGE_2_NEW" "$HME_HOME_DIR/.nix-profile"
 
 echo "$HME_TEMP_DIR"
 
