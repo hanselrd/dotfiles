528067860e5f646e99d93175a20d1540f51c9286
Hansel De La Cruz <18725263+hanselrd@users.noreply.github.com>
2023-09-03 21:34:54 -0400

    fix current time functions

--- lib/common.nix
+++ lib/common.nix
@@ -12,11 +12,13 @@
     )
   );
 
-  currentTimePretty = builtins.replaceStrings ["\n"] [""] (
-    builtins.readFile (
-      pkgs.runCommand "current-time-pretty" {
-        currentTime = builtins.currentTime;
-      } "date \"+%Y-%m-%dT%H:%M:%S%z %Z\" > $out"
-    )
-  );
+  currentTimePretty = tz:
+    builtins.replaceStrings ["\n"] [""] (
+      builtins.readFile (
+        pkgs.runCommand "current-time-pretty" {
+          buildInputs = [pkgs.tzdata];
+          currentTime = builtins.currentTime;
+        } "TZ=${tz} date \"+%Y-%m-%dT%H:%M:%S%z %Z\" > $out"
+      )
+    );
 }
