18385784f1bbdd5b31cc74a4f553054d4ab78051
Hansel De La Cruz <18725263+hanselrd@users.noreply.github.com>
2023-09-14 01:53:14 -0400

    update homeage

--- scripts/homeage-key.sh
+++ scripts/homeage-key.sh
@@ -5,4 +5,6 @@ if [ -f "$HOME/.nix-profile/etc/profile.d/nix.sh" ]; then
     . "$HOME/.nix-profile/etc/profile.d/nix.sh"
 fi
 
+mkdir -p "$HOME/.keys"
+chmod go-rwx "$HOME/.keys"
 age -d -i core/user/role/homeage/keys/0.age core/user/role/homeage/keys/1.age > "$HOME/.keys/2.age"
