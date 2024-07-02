874b490106592528ca6b505c29720337a11a6946
Hansel De La Cruz <18725263+hanselrd@users.noreply.github.com>
2024-07-02 00:35:49 -0400

    update windows bootstrap

--- cmd/windows/bootstrap.go
+++ cmd/windows/bootstrap.go
@@ -1,9 +1,13 @@
 package windows
 
 import (
+	"fmt"
+
+	"github.com/samber/lo"
 	"github.com/spf13/cobra"
 
 	"github.com/hanselrd/dotfiles/internal/shell"
+	"github.com/hanselrd/dotfiles/pkg/environment"
 )
 
 var bootstrapCmd = &cobra.Command{
@@ -14,8 +18,16 @@ var bootstrapCmd = &cobra.Command{
 		shell.Shell(
 			"powershell.exe -Command \"& {Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))}\"",
 		)
-		shell.Shell("choco.exe upgrade all -y")
-		// shell.Shell("choco.exe install starship --force -y")
+		shell.Shell("choco.exe upgrade all {{.VerbosityVerboseShort}} -y")
+		// shell.Shell("choco.exe install starship {{.VerbosityVerboseShort}} --force -y")
+		winUserName := lo.T2(lo.Must2(shell.Shell("powershell.exe '$env:UserName'"))).A
+		shell.Shell(
+			fmt.Sprintf(
+				"cp {{.VerbosityVerboseShortN}} %s/.config/starship.toml /mnt/c/Users/%s/.config/starship.toml",
+				environment.Environment.User.HomeDirectory,
+				winUserName,
+			),
+		)
 	},
 }
 
