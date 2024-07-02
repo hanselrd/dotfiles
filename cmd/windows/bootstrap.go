0e030da4c2dc5a2bc0189448afad47f899575bae
Hansel De La Cruz <18725263+hanselrd@users.noreply.github.com>
2024-07-01 23:57:47 -0400

    add windows commands

--- cmd/windows/bootstrap.go
+++ cmd/windows/bootstrap.go
@@ -1,9 +1,9 @@
 package windows
 
 import (
-	"log/slog"
-
 	"github.com/spf13/cobra"
+
+	"github.com/hanselrd/dotfiles/internal/shell"
 )
 
 var bootstrapCmd = &cobra.Command{
@@ -11,7 +11,11 @@ var bootstrapCmd = &cobra.Command{
 	Short: "Bootstrap command",
 	Long:  "Bootstrap command",
 	Run: func(cmd *cobra.Command, args []string) {
-		slog.Info("bootstrap called")
+		shell.Shell(
+			"powershell.exe -Command \"& {Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))}\"",
+		)
+		// shell.Shell("choco.exe upgrade chocolatey")
+		// shell.Shell("choco.exe upgrade all -y")
 	},
 }
 
