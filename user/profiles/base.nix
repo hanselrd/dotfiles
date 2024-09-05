84020dada9c22a8b680bb2266a202c59a0b45aab
Hansel De La Cruz <18725263+hanselrd@users.noreply.github.com>
2024-09-05 01:30:36 -0400

    fix base profile logic

--- user/profiles/base.nix
+++ user/profiles/base.nix
@@ -5,20 +5,20 @@
   env,
   ...
 }:
-{
-  roles.user.bootstrap.enable = true;
-  roles.user.scripts.enable = true;
-  roles.user.editor.enable = true;
-  roles.user.nix.enable = true;
-  roles.user.pager.enable = true;
-  roles.user.ripgrep.enable = true;
-  roles.user.shell.enable = true;
-  roles.user.theme.enable = true;
-  roles.user.zzz.enable = true;
-}
-// lib.optionalAttrs (!env.extra.encrypted) {
-  # roles.user.rts.enable = true;
-}
+(lib.recursiveUpdate {
+    roles.user.bootstrap.enable = true;
+    roles.user.scripts.enable = true;
+    roles.user.editor.enable = true;
+    roles.user.nix.enable = true;
+    roles.user.pager.enable = true;
+    roles.user.ripgrep.enable = true;
+    roles.user.shell.enable = true;
+    roles.user.theme.enable = true;
+    roles.user.zzz.enable = true;
+  }
+  (lib.optionalAttrs (!env.extra.encrypted) {
+    # roles.user.rts.enable = true;
+  }))
 // {
   home.packages = with pkgs; [
     age
