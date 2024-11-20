7fbcae2db70b1854a56a85cd6fd4b8161b31dadc
Hansel De La Cruz <18725263+hanselrd@users.noreply.github.com>
2024-11-20 00:11:31 -0500

    update flake

--- flake.nix
+++ flake.nix
@@ -2,8 +2,7 @@
   description = "dotfiles";
 
   inputs = {
-    # nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    nixpkgs.url = "github:NixOS/nixpkgs/8809585e6937d0b07fc066792c8c9abf9c3fe5c4";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     # nixpkgs.follows = "chaotic/nixpkgs";
 
     chaotic = {
