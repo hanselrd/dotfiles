656536d37c529bcf48d1c860e411632ade5974bc
Hansel De La Cruz <18725263+hanselrd@users.noreply.github.com>
2025-12-15 13:28:26 -0500

    update flake

--- scripts/nix-bootstrap.sh
+++ scripts/nix-bootstrap.sh
@@ -4,13 +4,18 @@ set -xe
 . lib/common.sh
 
 NIX_TEMP_DIR=$(make_temp_dir "nix.XXXXXX")
+NIX_VERSION=2.32.4
 
 rm -rf "$HOME/.nix-profile"
 mkdir -p "$HOME/.config/nix"
 rm -rf "$HOME/.config/nix/nix.conf"
-printf "experimental-features = nix-command flakes\nsandbox = true\nshow-trace = true\n" > "$HOME/.config/nix/nix.conf"
+printf "%s\n" \
+  "experimental-features = nix-command flakes pipe-operators" \
+  "sandbox = true" \
+  "show-trace = true" \
+  > "$HOME/.config/nix/nix.conf"
 
-curl -Lo "$NIX_TEMP_DIR/install.sh" https://nixos.org/nix/install
+curl -Lo "$NIX_TEMP_DIR/install.sh" "https://releases.nixos.org/nix/nix-$NIX_VERSION/install"
 chmod +x "$NIX_TEMP_DIR/install.sh"
 "$NIX_TEMP_DIR/install.sh" --no-daemon
 
