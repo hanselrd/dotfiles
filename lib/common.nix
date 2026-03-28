d8ccec5ea08ce397e4d1e727ea08496048d408ed
Hansel De La Cruz <18725263+hanselrd@users.noreply.github.com>
2026-03-28 13:13:47 -0400

    update flake

--- lib/common.nix
+++ lib/common.nix
@@ -191,7 +191,7 @@ rec {
     pkgs.buildGoModule {
       name = "dotfiles-go-bin-${name}";
       src = rootPath;
-      vendorHash = "sha256-GXtHPlJHMjABAdiHjutb0yM9yW2SU4ES51FpCIOIaEg=";
+      vendorHash = "sha256-jd0RXhdX1hU4RX2vtacfP6F+5jRTmruXUrHik92yEYw=";
       subPackages = [ "cmd/${name}" ];
       goSum = rootPath + "/go.sum";
       ldflags = [
