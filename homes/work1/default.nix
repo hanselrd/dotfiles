d3769c7e716e888d0944eae7ac5f3dabcb47795b
Hansel De La Cruz <18725263+hanselrd@users.noreply.github.com>
2026-06-14 13:25:23 -0400

    update work1

--- homes/work1/default.nix
+++ homes/work1/default.nix
@@ -2,6 +2,9 @@
   imports = [
     (homeModulesPath + "/common")
     (homeModulesPath + "/bash-to-zsh-override")
+    (homeModulesPath + "/development/cpp")
+    (homeModulesPath + "/development/java")
+    (homeModulesPath + "/development/python")
   ];
 
   home.stateVersion = "26.05";
